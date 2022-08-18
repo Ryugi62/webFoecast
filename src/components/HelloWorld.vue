d5285f637dd861d3f82f0a114392dbc3fe74b4f5
Ryugi62 <xorjf1027@naver.com>
2022-08-18 16:15:24 +0900

    드래그로 직선 그리는 기능

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
-    <cavnas id="drawCanvas" width="1224" height="768"></cavnas>
+    <canvas id="drawCanvas" width="1224" height="768" @mousedown="mosueDownCanvas($event)"
+      @mousemove="mouseOverCanvas($event)" @mouseup="mouseUpCanvas()"></canvas>
+    <div class="modeBox">
+      <div class="lineModeBox" :class="{ online: mode === 'line' }" @click="changeMode('line')">line</div>
+      <div class="curveModeBox" :class="{ online: mode === 'curve' }" @click="changeMode('curve')">curve</div>
+      <div class="textModeBox" :class="{ online: mode === 'text' }" @click="changeMode('text')">text</div>
+    </div>
   </div>
 </template>
 
@@ -8,34 +14,131 @@
 export default {
   name: 'HelloWorld',
 
+  data() {
+    return {
+      mode: "line",
+      canvas: null,
+      isDraw: false,
+      startPosition: {
+        x: 0,
+        y: 0
+      },
+      endPosition: {
+        x: 0,
+        y: 0
+      },
+      lineSave: []
+    }
+  },
+
   mounted() {
     this.init()
   },
 
   methods: {
     init() {
+      console.log(new Date(), "HelloWorld is mounted");
       this.createCanvas()
     },
 
     createCanvas() {
-      const canvas = document.getElementById('drawCanvas');
+      this.canvas = document.getElementById("drawCanvas")
+      this.canvas.width = 900;
+      this.canvas.height = 700;
+      this.canvas.style.border = "1px solid black";
 
-      console.log(canvas)
+      this.drawBgImage();
+    },
 
-      // canvas.style.border = "1px solid black";
+    drawBgImage() {
+      const ctx = this.canvas.getContext("2d");
+      const background = new Image();
+      background.src = "https://contents.creators.mypetlife.co.kr/content/uploads/2020/06/20005826/1838_3986_3444.jpg";
+      background.onload = function () {
+        ctx.drawImage(background, 150, 0);
+      }
+    },
 
-      // const ctx = canvas.getContext("2d");
-      // ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-      // ctx.fillRect(100, 100, 200, 200);
-      // ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
-      // ctx.fillRect(150, 150, 200, 200);
-      // ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-      // ctx.fillRect(200, 50, 200, 200);
+    mosueDownCanvas(event) {
+      this.isDraw = true;
+      this.startPosition.x = event.layerX;
+      this.startPosition.y = event.layerY;
+    },
+
+    async mouseOverCanvas(event) {
+      if (this.isDraw) {
+        this.endPosition.x = event.layerX
+        this.endPosition.y = event.layerY
+
+        switch (this.mode) {
+          case "line":
+            this.clearCanvas();
+            await this.drawBgImage();
+            this.getBeforeLine();
+            this.drawLine(this.startPosition.x, this.startPosition.y, this.endPosition.x, this.endPosition.y)
+            break;
+          case "curve":
+
+            break;
+          case "text":
+
+            break;
+        }
+      }
+    },
+
+    clearCanvas() {
+      this.canvas.getContext("2d").clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    getBeforeLine() {
+      this.lineSave.forEach(line => {
+        this.drawLine(line.startX, line.startY, line.endX, line.endY)
+      });
+    },
+
+    drawLine(startX, startY, endX, endY) {
+      const ctx = this.canvas.getContext("2d")
+
+      ctx.beginPath();
+      ctx.moveTo(startX, startY);
+      ctx.lineTo(endX, endY);
+      ctx.stroke();
+      ctx.closePath();
+    },
+
+    mouseUpCanvas() {
+      const line = { startX: this.startPosition.x, startY: this.startPosition.y, endX: this.endPosition.x, endY: this.endPosition.y }
+
+      this.lineSave.push(line)
+      this.isDraw = false
+
+      console.log(this.lineSave);
+    },
+
+    changeMode(mode) {
+      this.mode = mode
     }
-  },
-
+  }
 }
 </script>
 
 <style scoped>
+.hello {
+  display: flex;
+}
+
+canvas {
+  margin: auto;
+}
+
+.modeBox *:hover {
+  cursor: pointer;
+  background: gray;
+  ;
+}
+
+.online {
+  background: pink;
+}
 </style>
